9d29f1b80c76c6eac3e8a46c9c84c590cebf351a
apolinex <jose.teixeira@abo.fi>
2024-04-07 22:04:41 +0300

    Finally first testcase runs

--- testScrapLog.sh
+++ testScrapLog.sh
@@ -1,7 +1,39 @@
+#!/bin/bash
 echo "Is scrapLog.py doing what is suposed to do?"
 echo "This tests scrapLog.py by executing it againt input files in ./test-data/"
 echo "part of the ScrapLogGit2Net open-source project"
 echo "Developed by Jose Teixeira <jose.teixeira@abo.fi> "
 
+echo "" 
+echo "Testing with test-data/tensorFlowGitLog-3-commits-0-edges.IN" 
+echo "./scrapLog.py  -r test-data/tensorFlowGitLog-3-commits-0-edges.IN >> testResults.tmp"
 
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
 
+# TEST CASE 1
+# Input with 3 commits but not edges associating developers 
+
+./scrapLog.py  -r test-data/tensorFlowGitLog-3-commits-0-edges.IN >> testResults.tmp
+echo "Last line of output shoul be:"
+echo "Output should be ERROR collaboration tuplesList is empty !!"
+lastline=$(tail -n1 testResults.tmp)
+expectedLastLine="ERROR collaboration tuplesList is empty"
+
+#echo lastline=[$lastline]
+#echo expextedLastLine=[$expectedLastLine]
+
+# Test if "ERROR collaboration tuplesList is empty" is on the last line of the scrapLog STDOUT 
+
+echo 
+if [[  "$lastline" =~ "$expectedLastLine" ]]; then
+    echo "${GREEN}TESTCASE 1 passed${NC}"
+else
+    echo "${RED}TESTCASE 1 did not pass${NC}"
+    echo "ScrapLog should end with 'ERROR collaboration tuplesList is empty expected'"
+    
+fi
+
+echo 
+rm testResults.tmp
