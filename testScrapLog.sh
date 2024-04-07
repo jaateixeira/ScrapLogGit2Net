2154a06cb10dc8ab9ce25660f796beabc8d34d3e
apolinex <jose.teixeira@abo.fi>
2024-04-07 20:50:53 +0300

    Added header to testSrapLog

---
+++ testScrapLog.sh
@@ -0,0 +1,7 @@
+echo "Is scrapLog.py doing what is suposed to do?"
+echo "This tests scrapLog.py by executing it againt input files in ./test-data/"
+echo "part of the ScrapLogGit2Net open-source project"
+echo "Developed by Jose Teixeira <jose.teixeira@abo.fi> "
+
+
+
