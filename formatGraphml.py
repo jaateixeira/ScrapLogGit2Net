d2bc2e77f16b9734c0c1562ec4d5b81028df9b44
apolinex <jose.teixeira@abo.fi>
2024-04-11 17:31:37 +0300

    give layour . export does not lose athribute data

--- formatGraphml.py
+++ formatGraphml.py
@@ -10,9 +10,45 @@ import networkx as nx
 
 G = nx.read_graphml('NetworkFile.graphML')
 
+
+
 print ("number_of_nodes="+str(G.number_of_nodes()))
 print ("number_of_edges="+str(G.number_of_edges()))
 
-print ()
 
 
+print ("Time to calculate centralities")
+
+degree_centrality = nx.centrality.degree_centrality(G)  # sort by de
+
+sorted_degree_centrality=(sorted(degree_centrality.items(), key=lambda item: item[1], reverse=True))
+
+print ("degree_centrality")
+print (degree_centrality)
+print ("sorted_degree_centrality")
+print (sorted_degree_centrality)
+
+
+
+
+options = { 
+    'node_size': 10,
+    'width': 1,
+    'node-color': 'blue',
+}
+
+
+print ("Saving circular layout")        
+nx.draw_circular(G, **options)
+plt.show()
+plt.savefig("Uncolored-Circular-Layout.png")
+
+print ("Saving centrality layout")        
+nx.draw_spring(G, **options)
+
+plt.show()
+plt.savefig("Uncolored-Centrality-Layout.png")
+
+
+print ("writing formatted NetworkFile.graphML")
+nx.write_graphml_lxml(G, "Formatted-NetworkFile.graphML")
