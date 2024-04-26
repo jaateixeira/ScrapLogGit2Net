56ff1fcad0f9cb3f2c08abb7df0089db1f6a038a
apolinex <jose.teixeira@abo.fi>
2024-04-26 12:07:34 +0300

    nice for 2019

--- formatAndVizGraphML.py
+++ formatAndVizGraphML.py
@@ -22,8 +22,8 @@ import numpy as np
 global prefix_for_figures_filenames 
 
 
-#top_firms_that_matter = ['google','microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
-top_firms_that_matter = ['microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
+top_firms_that_matter = ['google','microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
+#top_firms_that_matter = ['microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
 #top_firms_that_do_not_matter = ['users','tensorflow','google']
 top_firms_that_do_not_matter = ['users','tensorflow']
 
