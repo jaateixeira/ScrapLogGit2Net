1a3c3acb5edf8938b67dbfddff1119a08dcef771
apolinex <jose.teixeira@abo.fi>
2024-06-13 10:52:15 +0300

    renamed test data

--- transform-nofi-2-nofo-GraphML.py
+++ transform-nofi-2-nofo-GraphML.py
@@ -12,43 +12,41 @@
 #Example of use verbose,fitering and only top firms
 # ./transform-nofi-2-nofo-GraphML.py -v test-data/icis-2024-wp-networks-graphML/tensorFlowGitLog-2022-git-log-outpuyt-by-Jose.IN.NetworkFile.graphML
 
+print ("")
 print ("transform-nofi-2-nofo-GraphML.py - transforming unweighted networks of individuals into weighted networks of organizations since June 2024")
+print ("Let's go")
+print ("")
 
-exit()
 
+"we need to handle networks"
 import networkx as nx
+
+"we need system and operating systems untils"
 import sys
-import argparse
 import os
 
-global out_file_name
+"we need to parse arguments" 
+import argparse
 
 
 
-top_firms_that_matter = ['google','microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
-#top_firms_that_matter = ['microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
-#top_firms_that_do_not_matter = ['users','tensorflow','google']
-top_firms_that_do_not_matter = ['users','tensorflow','gmail']
+"filtering of firms is not implemented yet" 
+# top_firms_that_matter = ['google','microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
+# top_firms_that_matter = ['microsoft','ibm','amazon','intel','amd','nvidia','arm','meta','bytedance']
+# top_firms_that_do_not_matter = ['users','tensorflow','google']
+# top_firms_that_do_not_matter = ['users','tensorflow','gmail']
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("file", type=str, help="the network file")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity")
+
 parser.add_argument("-t", "--top-firms-only", action="store_true",
                     help="only top_firms_that_matter")
 
 parser.add_argument("-f", "--filter-by-org", action="store_true",
                     help="top_firms_that_do_not_matter")
-
-parser.add_argument("-s", "--show", action="store_true",
-                    help="show the visualization, otherwises saves to png and pdf")
-
-parser.add_argument("-l", "--legend", action="store_true",
-                    help="adds a legend to the sociogram")
-
-parser.add_argument("-r", "--outside-legend-right", action="store_true",
-                            help="the legend to the sociogram goes outside to the right")
-
 
 args = parser.parse_args()
 
@@ -61,15 +59,11 @@ if args.verbose:
 if args.top_firms_only:
     print()
     print("In top-firms only mode")
-    print()
+    print("Not implemented yet")
+    sys.exit()
 
 if  args.filter_by_org:
     print()
     print("In filtering by org mode")
-    print()
-
-
-if args.show:
-    print()
-    print("In snow mode")
-    print()
+    print("Not implemented yet")
+    sys.exit()
