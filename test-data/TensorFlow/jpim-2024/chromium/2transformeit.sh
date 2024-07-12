e146adf18939415932cba76bdefa3e55b916e161
apolinex <jose.teixeira@abo.fi>
2024-07-12 21:28:14 +0300

    Not working well

---
+++ test-data/TensorFlow/jpim-2024/chromium/2transformeit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+FILTERED_FILE=tensorFlowGitLog-all-till-12-Apri-2024.NetworkFile-filtered.graphML 
+INPUT=$FILTERED_FILE
+
+TRANSFORMED_FILE=tensorFlowGitLog-all-till-12-Apri-2024.NetworkFile-filtered-transformed-to-nofo.graphML
+OUTPUT=$TRANSFORMED_FILE
+
+
+echo -e  "Transforming network $INPUT" "\n"
+
+FOCAL_ORG=chromium
+
+TOP10_ORG=google,intel,nvidia,arm,ibm,amd,microsoft,huawei,amazon,naver
+
+du -sh $INPUT
+
+echo -r "Taking $FOCAL_ORG as the network we are transforming" "\n"
+
+CMD="../../../../transform-nofi-2-nofo-GraphML.py -s $INPUT"
+
+
+echo -e "Excecutiong:\n  $CMD \n"
+
+eval $CMD
+
+echo -e "TESTED" "Worked" "\n"
+
+du -sh $INPUT
+
+
+echo -e "Network is transformed, let's now see  the network \n"
+./../../../formatFilterAndViz-nofo-GraphML.py -l $OUTPUT
+
+
+
+
