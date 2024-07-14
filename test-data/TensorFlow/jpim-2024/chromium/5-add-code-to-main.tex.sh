89e78a3aca65f81a9fc96c91b7ec3802a3266d78
apolinex <jose.teixeira@abo.fi>
2024-07-14 21:37:16 +0300

    creating a 5th file

--- test-data/TensorFlow/jpim-2024/chromium/5-add-code-to-main.tex.sh
+++ test-data/TensorFlow/jpim-2024/chromium/5-add-code-to-main.tex.sh
@@ -76,47 +76,6 @@ echo $CMD
 eval $CMD
 echo ""
 
-echo "Now check than main exists" 
-
-MAIN_FILE=main.tex
-
-# Check if the file is provided
-if [ -f OVERLEAF_FIGURES_FOLDERS/$MAIN_FILE ]; then
-    echo "main.tex missing"
-    exit 1
-fi
-
-echo "File is provided"
-
-# LaTeX code to be inserted
-FIGURE_CODE="\n\\begin{figure}[h!]\n\\centering\n\\includegraphics[width=0.8\\textwidth]{test.pdf}\n\\caption{Description of the figure}\n\\label{fig:test}\n\\end{figure}\n"
-
-echo "FIGURE_CODE=$FIGURE_CODE"
-
-# Use sed to insert the figure code after the specified line
-sed -i "/\\subsection{BOT added figures}/a $FIGURE_CODE" "$MAIN_FILE"
-
-
-
-# Use sed to insert the figure code after the specified line
-#CMD="sed -i /\\subsection{BOT added figures}/a $FIGURE_CODE $MAIN_FILE" 
-
-echo $CMD
-eval $CMD
-echo ""
-
-
-echo "Figure code inserted into $MAIM_FILE"
-
-
-echo "Testing if there with grep "
-
-CMD="grep fig:$FOCAL_ORG $MAIN_FILE" 
-
-echo $CMD
-eval $CMD
-echo ""
-
 
 echo "back to origin" 
 cd - 
