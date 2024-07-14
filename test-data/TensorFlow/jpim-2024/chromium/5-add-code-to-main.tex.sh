ba709cc38a7accce892f565fc2bf5a98cb8a6682
apolinex <jose.teixeira@abo.fi>
2024-07-14 22:07:10 +0300

    Should learn SED

--- test-data/TensorFlow/jpim-2024/chromium/5-add-code-to-main.tex.sh
+++ test-data/TensorFlow/jpim-2024/chromium/5-add-code-to-main.tex.sh
@@ -10,6 +10,10 @@ echo $CMD
 eval $CMD
 echo ""
 
+
+echo "I am at PWD=$PWD"
+
+
 OVERLEAF_FIGURES_FOLDERS=/home/apolinex/rep_clones/floss_sna_team/TensorFlowSocialStructure/tensorflowsna-open-coopetition-triple-helix-non-commercial/Figures/noo/
 
 
@@ -17,11 +21,10 @@ OVERLEAF_FIGURES_FOLDERS=/home/apolinex/rep_clones/floss_sna_team/TensorFlowSoci
 
 echo "Testing if $FOCAL_ORG pdf figure is there"
 
-CMD="du -sh OVERLEAF_FIGURES_FOLDERS/$FOCAL_ORG""_croped.pdf"
+CMD="du -sh $OVERLEAF_FIGURES_FOLDERS$FOCAL_ORG""_cropped.pdf"
 echo $CMD
 eval $CMD
 echo ""
-
 
 
 
@@ -29,8 +32,15 @@ echo "Now check than main exists"
 
 MAIN_FILE=main.tex
 
+
+CMD="du -sh main.tex & file main.tex"
+echo $CMD
+eval $CMD
+echo ""
+
+
 # Check if the file is provided
-if [ -f OVERLEAF_FIGURES_FOLDERS/$MAIN_FILE ]; then
+if [ -z $MAIN_FILE ]; then
     echo "main.tex missing"
     exit 1
 fi
@@ -38,17 +48,26 @@ fi
 echo "File is provided"
 
 # LaTeX code to be inserted
-FIGURE_CODE="\n\\begin{figure}[h!]\n\\centering\n\\includegraphics[width=0.8\\textwidth]{test.pdf}\n\\caption{Description of the figure}\n\\label{fig:test}\n\\end{figure}\n"
+FIGURE_CODE="\n\\begin{figure}[h!]\n\\centering\n\\includegraphics[width=0.8\\textwidth]{./Figures/noo/$FOCAL_ORG""_cropped.pdf}\n\\caption{$FOCAL_ORG inter-organizational network deployed by ScrapLog}\n\\label{fig:noo_$FOCAL_ORG}\n\\end{figure}\n"
 
 echo "FIGURE_CODE=$FIGURE_CODE"
+echo ""
+
+echo "running sed to add the code"
+
+echo "----- "
+
+#echo sed -i "/\\subsection{BOT added figures}/a $FIGURE_CODE" "$MAIN_FILE"
+#sed -i "/\\subsection{BOT added figures}/a $FIGURE_CODE" "$MAIN_FILE"
+
+
+echo  $FIGURE_CODE >> "$MAIN_FILE"
+
+echo "-----"
+echo ""
 
 # Use sed to insert the figure code after the specified line
-sed -i "/\\subsection{BOT added figures}/a $FIGURE_CODE" "$MAIN_FILE"
-
-
-
-# Use sed to insert the figure code after the specified line
-#CMD="sed -i /\\subsection{BOT added figures}/a $FIGURE_CODE $MAIN_FILE" 
+#CMD="sed -i /\\subsection{BOT added figures}/a $FIGURE_CODE" "$MAIN_FILE"
 
 echo $CMD
 eval $CMD
@@ -56,6 +75,7 @@ echo ""
 
 
 echo "Figure code inserted into $MAIM_FILE"
+echo ""
 
 
 echo "Testing if there with grep "
@@ -65,7 +85,6 @@ CMD="grep fig:$FOCAL_ORG $MAIN_FILE"
 echo $CMD
 eval $CMD
 echo ""
-
 
 
 echo "back to origin" 
