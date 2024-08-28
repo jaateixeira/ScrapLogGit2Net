a551e002eb5f845c8344f32859c93e2807a3a59a
apolinex <jose.teixeira@abo.fi>
2024-08-28 13:18:52 +0300

    added filterd file

--- test-data/home-assistant-core/1-get_network_visualizations.sh
+++ test-data/home-assistant-core/1-get_network_visualizations.sh
@@ -13,18 +13,34 @@ fi
 source config.cfg
 source utils.sh 
 
-echo  e "Invoking the $FFV_NO_FI_GRAPHML_SCRIPT with save_graphML so we can later transform it"
-
-CMD="$FFV_NO_FI_GRAPHML_SCRIPT  $INPUT pl oi $COMPANIES_TO_IGNORE save_graphML" 
 
 
-echo e "Excecutiong:\n  $CMD \n"
+file_exists_and_is_not_empty  "$INPUT"
+print_happy_smile
+command_exists "$FFV_NO_FI_GRAPHML_SCRIPT"
+print_happy_smile 
+
+echo ""
+echo "1) VAR FFV_NO_FI_GRAPHML_SCRIPT= $FFV_NO_FI_GRAPHML_SCRIPT"
+echo "2) VAR INPUT = $INPUT"
+echo "3) VAR COMPANIES_TO_IGNORE = $COMPANIES_TO_IGNORE"
+echo "4) VAR TOP_10_COMPANIES $TOP_10_COMPANIES"
+echo ""
+
+echo -e "Invoking the $FFV_NO_FI_GRAPHML_SCRIPT with save_graphML so we can later transform it"
+
+CMD="$FFV_NO_FI_GRAPHML_SCRIPT  $INPUT -pl --org_list_to_ignore $COMPANIES_TO_IGNORE --org_list_only $TOP_10_COMPANIES --save_graphML" 
+
+
+echo -e "Excecutiong:\n  $CMD \n"
 
 eval $CMD
 
-echo e "TESTED" "Worked" "\n"
+echo -e "TESTED" "Worked" "\n"
 
-echo e "Filtered file saved at $FILTERED_FILE" "\n"
+echo -e "Filtered file saved at $FILTERED_FILE" "\n"
+
+file_exists_and_is_not_empty  "$INPUT"
 
 
 du sh $INPUT
