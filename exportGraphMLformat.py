36504da874b39032c711687a6bcc61f441a8862f
Syed Sabbir Ahmed <138389376+SabbirGit@users.noreply.github.com>
2024-11-14 20:40:58 +0200

    exportgraphmlformat mod

--- exportGraphMLformat.py
+++ exportGraphMLformat.py
@@ -1,128 +1,55 @@
-# file to export VCS (CVS,SVN, GIT) SNA data to the graphml format used by visone and gephi
-
-#import sys
-
-#from __future__ import absolute_import
-#from __future__ import print_function
-
 from datetime import date
 
-
-
-graphml_header = '<?xml version="1.0" encoding="UTF-8"?>\n<!-- This file was created by scraplog.py script for OSS SNA research purposes --> \n'  + '<!-- For more information contact jose.teixeira@utu.fi and check www.jteixeira.eu for more information on OSS SNA research -->\n' + '<graphml xmlns="http://graphml.graphdrawing.org/xmlns"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' + 'xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n' 
+graphml_header = '''<?xml version="1.0" encoding="UTF-8"?>
+<!-- This file was created by scraplog.py script for OSS SNA research purposes --> 
+<!-- For more information contact jose.teixeira@utu.fi and check www.jteixeira.eu for more information on OSS SNA research -->
+<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">
+'''
 
 graphml_closer = "</graphml>\n"
+graph_opener = '<graph id="G" edgedefault="undirected">\n'
+graph_closer = '</graph>\n'
 
-graph_opener= '<graph id="G" edgedefault="undirected">\n'
-graph_closer= '</graph>'
-
-
-# Set atributes that a node can have 
-# i.e <key id="d0" for="node" attr.name="color" attr.type="string"> </key>
-def setNodeAntributeKey(id, attrName, attrType):
-
-    if attrType != "string":
-        print ("Not supported GRAPHML node atribute type")
-        sys.exit()
+def set_node_attribute_key(attr_id, attr_name, attr_type="string"):
+    """Set attributes for a node in GraphML."""
+    if attr_type != "string":
+        raise ValueError("Only 'string' type is supported for GraphML node attributes.")
     
-    res = '<key id="d'+str(id)+'" for="node" attr.name="'+attrName+'" attr.type="'+attrType+'"> \n' 
+    return (f'<key id="d{attr_id}" for="node" attr.name="{attr_name}" attr.type="{attr_type}">\n'
+            f'\t<default>DEFAULT{attr_name}</default>\n</key>\n')
 
-    res +='\t<default>DEFAULT'+attrName+'</default>'
-    res +='</key>\n' 
+def add_node(node_id, attributes):
+    """Add a node with attributes."""
+    attribute_data = ''.join(f'\t<data key="d{attr_id}">{value}</data>\n' for attr_id, value in attributes)
+    return f'\t<node id="{node_id}">\n{attribute_data}\t</node>\n'
 
-    return res
-
-# Add a node with a tuple of atributes
-def addNode(nodeId, atributes):
-    
-    res = '\t<node id="'+ str(nodeId) +'">\n'
-
-    for atribute in atributes:
-        key = "d"+str(atribute[0])
-        value = atribute[1]
-
-        res += '\t<data key="'+key+'">'+value+'</data>\n'
-    
-    res+='\t</node>\n' 
-    return res 
- 
-# Add a edge connecting nodes 
-def addEdge(edgeId,nodeIdFrom,nodeIdTo):
-
-    if edgeId[0] != 'e':
-        print ("edgeid mist start with e: ie. e0, e12")
-        sys.exit()
-    
-    return  '\t<edge id="'+str(edgeId)+'" source="'+ str(nodeIdFrom)+'" target="'+ str(nodeIdTo)+'"/>\n'
-
-
-
-# Sample ML file works on Visone and Gephi
+def add_edge(edge_id, node_id_from, node_id_to):
+    """Add an edge connecting two nodes."""
+    if not edge_id.startswith('e'):
+        raise ValueError("Edge ID must start with 'e', e.g., e0, e12.")
+    return f'\t<edge id="{edge_id}" source="{node_id_from}" target="{node_id_to}"/>\n'
 
 def main():
-    #prints a smaple graphML file using this file functions  
+    """Generate and print a sample GraphML file."""
     print(graphml_header)
-    print(setNodeAntributeKey(0,"e-mail","string"))
-    print(setNodeAntributeKey(1,"color","string"))
-    print(setNodeAntributeKey(2,"affiliation","string"))
+    print(set_node_attribute_key(0, "e-mail"))
+    print(set_node_attribute_key(1, "color"))
+    print(set_node_attribute_key(2, "affiliation"))
     print(graph_opener)
 
-    # Adding a node with email atribute
-    print(addNode(0,[(0,"jose@webkit.org")]))
+    # Add nodes with attributes
+    print(add_node(0, [(0, "jose@webkit.org")]))
+    print(add_node(1, [(0, "martin@svh.com"), (1, "turquoise"), (2, "San Vicent Health")]))
+    print(add_node(2, [(0, "tt@utu.fi"), (1, "red"), (2, "University of Turku")]))
 
-    # Add nodes with email, color and affiliation atribute
+    # Add edges
+    print(add_edge("e0", 0, 1))
+    print(add_edge("e1", 0, 2))
 
-    print(addNode(1,[(0,"martin@svh.com"),(1,"turquoise"),(2,"San Vicent Health")]))
-    print(addNode(2,[(0,"tt@utu.fi"),(1,"red"),(2,"University of Turku")]))
-
-    print(addEdge("e0",0,1))
-    print(addEdge("e1",0,2))
     print(graph_closer)
     print(graphml_closer)
-
-
-    print ("exportGraphml imported")
-
-if __name__ == "__main__":
-    main()
- # file to export VCS (CVS,SVN, GIT) SNA data to the graphml format used by visone and gephi
- 
-
-
- 
-graphml_header = '<?xml version="1.0" encoding="UTF-8"?>\n<!-- This file was created by scraplog.py script for OSS SNA research purposes --> \n'  + '<!-- For more information contact jose.teixeira@utu.fi and check www.jteixeira.eu for more information on OSS SNA research -->\n' + '<graphml xmlns="http://graphml.graphdrawing.org/xmlns"  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' + 'xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n' 
-
-def main():
-     #prints a smaple graphML file using this file functions  
-    print(graphml_header)
-    print(setNodeAntributeKey(0,"e-mail","string"))
-    print(setNodeAntributeKey(1,"color","string"))
-    print(setNodeAntributeKey(2,"affiliation","string"))
-    print((setNodeAntributeKey(0,"e-mail","string")))
-    print((setNodeAntributeKey(1,"color","string")))
-    print((setNodeAntributeKey(2,"affiliation","string")))
-    print(graph_opener)
- 
-     # Adding a node with email atribute
-    print(addNode(0,[(0,"jose@webkit.org")]))
-    print((addNode(0,[(0,"jose@webkit.org")])))
- 
-     # Add nodes with email, color and affiliation atribute
- 
-    print(addNode(1,[(0,"martin@svh.com"),(1,"turquoise"),(2,"San Vicent Health")]))
-    print(addNode(2,[(0,"tt@utu.fi"),(1,"red"),(2,"University of Turku")]))
-    print((addNode(1,[(0,"martin@svh.com"),(1,"turquoise"),(2,"San Vicent Health")])))
-    print((addNode(2,[(0,"tt@utu.fi"),(1,"red"),(2,"University of Turku")])))
- 
-    print(addEdge("e0",0,1))
-    print(addEdge("e1",0,2))
-    print((addEdge("e0",0,1)))
-    print((addEdge("e1",0,2)))
-    print(graph_closer)
-    print(graphml_closer)
- 
- 
     print("exportGraphml imported")
- 
+
 if __name__ == "__main__":
     main()
