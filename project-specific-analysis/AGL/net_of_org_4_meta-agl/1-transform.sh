811ed5048691cea39272945685c6a68855c10b27
Jose Teixeira <japolinario@gmail.com>
2025-05-07 21:20:18 +0300

    added link

---
+++ project-specific-analysis/AGL/net_of_org_4_meta-agl/1-transform.sh
@@ -0,0 +1,1 @@
+../../../transform-nofi-2-nofo-GraphML.py ../net_of_ind_4_meta-agl/agl_raw_logs_meta-agl_albacore_20250427_171634.txt.IN_albacore.txt.NetworkFile.graphML
---
+++ project-specific-analysis/AGL/net_of_org_4_meta-agl/2-viz.sh
@@ -0,0 +1,1 @@
+../../../formatFilterAndViz-nofo-GraphML.py -slt agl_raw_logs_meta-agl_albacore_20250427_171634.txt.IN_albacore.txt.NetworkFile-transformed-to-nofo.graphML
