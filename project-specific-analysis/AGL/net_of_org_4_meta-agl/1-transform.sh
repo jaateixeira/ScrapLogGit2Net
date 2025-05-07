2c4191c7f952dcdf26f5e6ae8807c449df1dafc2
Jose Teixeira <japolinario@gmail.com>
2025-05-07 21:51:19 +0300

    jose rules

--- project-specific-analysis/AGL/net_of_org_4_meta-agl/1-transform.sh
+++ project-specific-analysis/AGL/net_of_org_4_meta-agl/1-transform.sh
@@ -1,1 +1,1 @@
-../../../transform-nofi-2-nofo-GraphML.py ../net_of_ind_4_meta-agl/agl_raw_logs_meta-agl_albacore_20250427_171634.txt.IN_albacore.txt.NetworkFile.graphML
+../../../transform-nofi-2-nofo-GraphML.py agl_raw_logs_meta-agl_salmon_20250427_171908.txt.IN_salmon.txt.NetworkFile-filtered.graphML
--- project-specific-analysis/AGL/net_of_org_4_meta-agl/2-viz.sh
+++ project-specific-analysis/AGL/net_of_org_4_meta-agl/2-viz.sh
@@ -1,1 +1,1 @@
-../../../formatFilterAndViz-nofo-GraphML.py -slt agl_raw_logs_meta-agl_albacore_20250427_171634.txt.IN_albacore.txt.NetworkFile-transformed-to-nofo.graphML
+../../../formatFilterAndViz-nofo-GraphML.py -slt agl_raw_logs_meta-agl_salmon_20250427_171908.txt.IN_salmon.txt.NetworkFile-filtered-transformed-to-nofo.graphML
