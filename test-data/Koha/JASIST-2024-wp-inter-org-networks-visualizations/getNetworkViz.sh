fdc8b1f8c24040826fd0e65c63f0b835fe162091
Jose Teixeira <japolinario@gmail.com>
2025-06-01 22:58:49 +0300

    got the net viz script

--- test-data/Koha/JASIST-2024-wp-inter-org-networks-visualizations/getNetworkViz.sh
+++ test-data/Koha/JASIST-2024-wp-inter-org-networks-visualizations/getNetworkViz.sh
@@ -6,10 +6,10 @@
 VIZ=''../../../formatFilterAndViz-nofo-GraphML.py''
 VIZ_ARG='-sl'
 
-KOHA_NET_GraphML_PATH="../JASIST-2024-wp-networks-graphML"
+KOHA_NET_GraphML_PATH="../JASIST-2024-wp-inter-org-networks-graphML/"
 
-echo TRANSFORMER=$TRANSFORMER
-echo FILTER_ARG=$FILTER_ARG
+echo VIZ=$VIZ
+echo VIZ_ARG=$VIS_ARG
 echo KOHA_NET_GraphML_PATH=$KOHA_NET_GraphML_PATH
 
 cho Loop through files in the developer networks directory
@@ -20,7 +20,7 @@ for file in "$KOHA_NET_GraphML_PATH"/*; do
     if [[ $file == *.graphML ]]       #  this is the snag
               then
                     echo "Getting top10 for"  $file ":"
-                    CMD="$FILTER $FILTER_ARG $file"
+                    CMD="$VIZ $VIZ_ARG $file"
 
                     echo CMD = $CMD
 
