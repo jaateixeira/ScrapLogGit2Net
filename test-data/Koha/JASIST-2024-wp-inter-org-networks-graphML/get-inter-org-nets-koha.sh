28c371b1b107a8c9f2d89d6336fc5a7234ce3046
Jose Teixeira <japolinario@gmail.com>
2025-06-01 21:11:48 +0300

    calling trandormer

---
+++ test-data/Koha/JASIST-2024-wp-inter-org-networks-graphML/get-inter-org-nets-koha.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+TRANSFORMER=''../../../transform-nofi-2-nofo-GraphML.py''
+TRANSFORMER_ARG=''
+
+KOHA_NET_GraphML_PATH="../JASIST-2024-wp-networks-graphML"
+
+echo TRANSFORMER=$TRANSFORMER
+echo TRANSFORMER_ARG=$TRANSFORMER_ARG
+echo KOHA_NET_GraphML_PATH=$KOHA_NET_GraphML_PATH
+
+
+if [ ! "$BASH_VERSION" ] ; then
+    echo "Please do not use sh to run this script ($0), just execute it directly" 1>&2
+    exit 1
+fi
+
+
+
+
+echo Checking  if the target is not a directory
+if [ ! -d "$KOHA_NET_GraphML_PATH" ]; then
+  exit 1
+fi
+
+echo Loop through files in the developer networks directory
+for file in "$KOHA_NET_GraphML_PATH"/*; do
+  if [ -f "$file" ]; then
+    echo "$file"
+
+    if [[ $file == *.graphML ]]       #  this is the snag
+              then
+                    echo "Calling transformer" $file ":"
+                    echo
+              fi
+
+  fi
+done
+
+
+
+INPUT=$FILTERED_FILE
+
+OUTPUT=$TRANSFORMED_FILE
+
+
