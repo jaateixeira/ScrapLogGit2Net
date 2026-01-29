7a2fd41f673f952f5f48c0de9f101605dfd55c54
Jose Teixeira <japolinario@gmail.com>
2026-01-29 16:56:07 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scraplog-git2net",
+    version="0.1.0",
+    description="Transform Git logs into collaboration networks",
+    author="Your Name",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "networkx>=3.0",
+        "rich>=13.0",
+        "loguru>=0.7.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "scraplog=scraplog:main",
+            "transform-nofi-2-nofo=transform_nofi_2_nofo_GraphML:main",
+            "deanonymize-github-users=deanonymize_github_users:main",
+        ],
+    },
+    python_requires=">=3.8",
+)
