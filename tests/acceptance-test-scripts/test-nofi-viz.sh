576efe358d0cf47bbee2d1968b602ec60a609cd2
Jose Teixeira <japolinario@gmail.com>
2026-01-30 23:19:55 +0200

    asdded tests

---
+++ tests/acceptance-test-scripts/test-nofi-viz.sh
@@ -0,0 +1,3 @@
+./formatFilterAndViz-nofi-GraphML.py -pl test-data/2-org-with-2-developers-each-all-in-inter-firm-cooperation-relationships.graphML
+./formatFilterAndViz-nofi-GraphML.py -pl test-data/5-pentagon-with-star.graphML
+./formatFilterAndViz-nofi-GraphML.py -pl test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML
