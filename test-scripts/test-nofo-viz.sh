a02fb039585085758e89b82e2701a816a92c0193
Jose Teixeira <japolinario@gmail.com>
2026-01-30 22:21:16 +0200

    added script

---
+++ test-scripts/test-nofo-viz.sh
@@ -0,0 +1,1 @@
+./formatFilterAndViz-nofo-GraphML.py -v --show --legend --color_map business_firm_color_dictionary_json/firm_color_dict.json test-data/5-pentagon-with-star-transformed-to-nofo.graphML
