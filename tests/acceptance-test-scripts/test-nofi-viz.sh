c15beb1018f99b13b0b6b6129475b9ba58c722f2
Jose Teixeira <japolinario@gmail.com>
2026-01-31 20:09:54 +0200

    nofi acceptace test passed

--- tests/acceptance-test-scripts/test-nofi-viz.sh
+++ tests/acceptance-test-scripts/test-nofi-viz.sh
@@ -1,3 +1,3 @@
-./formatFilterAndViz-nofi-GraphML.py -pl test-data/2-org-with-2-developers-each-all-in-inter-firm-cooperation-relationships.graphML
-./formatFilterAndViz-nofi-GraphML.py -pl test-data/5-pentagon-with-star.graphML
-./formatFilterAndViz-nofi-GraphML.py -pl test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML
+./nofi_graphml_network_visualizer.py -pl test-data/2-org-with-2-developers-each-all-in-inter-firm-cooperation-relationships.graphML
+./nofi_graphml_network_visualizer.py -pl test-data/5-pentagon-with-star.graphML
+./nofi_graphml_network_visualizer.py -pl test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML
