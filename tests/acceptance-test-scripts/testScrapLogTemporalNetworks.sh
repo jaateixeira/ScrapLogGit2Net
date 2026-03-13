2ba7fc7ec168856205ec0c0f1e3c462ae731fd64
Jose Teixeira <jose.apolinarioteixeira@aalto.fi>
2026-03-13 22:04:29 +0200

    first acceptance test passes

--- tests/acceptance-test-scripts/testScrapLogTemporalNetworks.sh
+++ tests/acceptance-test-scripts/testScrapLogTemporalNetworks.sh
@@ -43,220 +43,34 @@ $TC1_command
 
 TC1_output_file="tensorFlowGitLog-temporal-2-developers-3-commits-same-file.temporal.graphml.zip"
 
-expected_pattern='<graph edgedefault="undirected">
+# Check if the file exists
+if [ ! -f "$TC1_output_file" ]; then
+    echo "Error: File $TC1_output_file not found."
+    exit 1
+fi
+
+expected_pattern=('<graph edgedefault="undirected">
     <node id="dasenov@google.com" />
     <node id="ddunleavy@google.com" />
     <edge source="dasenov@google.com" target="ddunleavy@google.com" id="0">
       <data key="d0">2024-01-03T04:05:02-08:00</data>
     </edge>
-  </graph>'
+  </graph>')
 
-echo expected_pattern=$expected_pattern
+echo expected_pattern="${expected_pattern[0]}"
 
-if zgrep -z -q -F "$expected_pattern" $TC1_output_file; then
-    echo "Found"
+if zgrep -z -q -F "${expected_pattern[0]}" $TC1_output_file; then
+     echo "Success: exported graphML.zip file had expected content"
+        echo "${GREEN}TESTCASE 1 passed${NC}"
 else
-    echo "Not found"
+     echo "${RED}TESTCASE 1 did not pass${NC}"
+    echo "ScrapLog should result in  ${expected_pattern[0]}"
 fi
 
+rm -v $TC1_output_file
 
 exit
 
-# Define the file to read
-file="testResults.tmp"
-
-# Check if the file exists
-if [ ! -f "$file" ]; then
-    echo "Error: File $file not found."
-    exit 1
-fi
-
-
-if assert_str_in_file "$expectedLastLine" "$file" ; then
-    echo "Success: The error message was found."
-        echo "${GREEN}TESTCASE 1 passed${NC}"
-else
-    echo "Failure: The error message was not found."
-    echo "${RED}TESTCASE 1 did not pass${NC}"
-    echo "ScrapLog should result in  $expectedLastLine"§
-    exit 1
-fi
-
-
-
-echo 
-rm testResults.tmp
-
-
-
-# TEST CASE 2
-# Input with 3 commits and one edge associating developers
-# lawrencews@google.com and olupton@nvidia.com co-edited tensorflow/core/lib/gtl/array_slice.h
-
-
-echo "" 
-echo "Testing with test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edges.IN"
-echo "./scrapLog.py  -r test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edge.IN > testResults.tmp"
-
-
-./scrapLog.py  -r test-data/TensorFlow/tensorFlowGitLog-3-commits-1-edge.IN > testResults.tmp
-
-# Define the file to read
-file="testResults.tmp"
-
-cat $file
-
-# Check if the file exists
-if [ ! -f "$file" ]; then
-    echo "Error: File $file not found."
-    exit 1
-fi
-# Check if the file contains the specific error message
-if assert_str_in_file "Network nodes (developers): 2" "$file" ; then
-    echo "Success: The error message was found."
-        echo "${GREEN}TESTCASE 2.1 passed${NC}"
-else
-    echo "Failure: The error message was not found."
-    echo "${RED}TESTCASE 2-1 did not pass${NC}"
-    echo "ScrapLog sould result in  'Number of unique collaborations (i.e., network edges)[1]'"
-    exit 1
-fi
-
-
-echo ""
-
-# Test if tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML was created
-echo ""
-
-GraphMLFILE="tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML"
-if test -f "$GraphMLFILE"; then
-    echo "${GREEN}TESTCASE 2.2 - $GraphMLFILE exists ${NC}"
-else
-    echo "${RED}TESTCASE 2.2 did not pass${NC}"
-    echo "$GraphMLFILE not created" 
-fi
-
-GraphMLHeader='<?xml version="1.0" encoding="UTF-8"?>
-<!-- This file was created by scraplog.py script for OSS SNA research purposes --> '
-
-# Test if GraphML have correct header
-actualHeader=$(head -2 $GraphMLFILE)
-
-echo ""
-#echo actualHeader=$actualHeader
-#echo GraphMLHeader=$GraphMLHeader
-
-
-if [ "$actualHeader" == "$GraphMLHeader" ]; then 
-   echo "${GREEN}TESTCASE 2.3 - $GraphMLFILE have a good header${NC}"
-else 
-   echo "${RED}TESTCASE 2.3 did not pass${NC}"
-   echo "$GraphMLFILE with wrong header" 
-fi
-
-
-
-# Test if nodes are correct
-echo ""
-
-if grep -q '<data key="d0">olupton@nvidia.com</data>' "$GraphMLFILE"; then
-    echo "${GREEN}TESTCASE 2.4 - $GraphMLFILE have the olupton@nvidia.com node as expected${NC}"
-else 
-    echo "${RED}TESTCASE 2.4 did not pass${NC}"
-fi
-
-echo "" 
-
-if grep -q '<data key="d0">lawrencews@google.com</data>' "$GraphMLFILE"; then
-    echo "${GREEN}TESTCASE 2.5 - $GraphMLFILE have the lawrencews@google.com node as expected${NC}"
-else 
-    echo "${RED}TESTCASE 2.5 did not pass${NC}"
-fi
-
-
-echo "" 
-# Test if edges are correct
-
-
-if grep -q '<edge id="e0" source="0" target="1"/>' "$GraphMLFILE"; then
-    echo "${GREEN}TESTCASE 2.6 - $GraphMLFILE have the the expected edge connecting two nodes${NC}"
-else 
-    echo "${RED}TESTCASE 2.6 did not pass${NC}"
-    echo '   expecting <edge id="e0" source="0" target="1"/>'
-    echo "   please check check the edges in  tensorFlowGitLog-3-commits-1-edge.NetworkFile.graphML"
-    
-fi
-
-
-echo "" 
-# Test if the graphML files closes with the right footer 
-
-cmdOutput=$(tail -1  $GraphMLFILE)
-
-
-if [ "$cmdOutput" == '</graph></graphml>' ]; then
-    echo "${GREEN}TESTCASE 2.7 - $GraphMLFILE ends in the proper way with </graphml>${NC}"
-else 
-    echo "${RED}TESTCASE 2.7 did not pass - Wrong ending of $GraphMLFILE ${NC}"
-fi
-
-
-
-# TEST CASE 3
-# Input with the tensorFlowGitLog-first-trimester-2024.IN
-
-
-echo "" 
-echo "Testing with tensorFlowGitLog-first-trimester-2024.IN - 32659 lines"
-echo "Should capture colllaboration between during first trimester 2024 in TensorFlow "
-echo "Should also filter the bots and emails listed in test-configurations/TensorFlowBots.txt"
-# Not passing configurations as they can evolve over time 
-echo "./scrapLog.py  -r test-data/TensorFlow/tensorFlowGitLog-first-trimester-2024.IN > testResults.tmp"
-
-
-./scrapLog.py  -r test-data/TensorFlow/tensorFlowGitLog-first-trimester-2024.IN > testResults.tmp
-
-echo""
-
-if grep -q 'Total commit blocks found: 4431' testResults.tmp; then
-    echo "${GREEN}TESTCASE 3.1 - Scrapped changelog blocks [4431] as expected${NC}"
-else 
-    echo "${RED}TESTCASE 3.1 did not pass - unexpected number of changelog blocks${NC}"
-fi
-
-
-echo""
-
-if grep -q 'Files affected: 8467' testResults.tmp; then
-    echo "${GREEN}TESTCASE 3.2 - number of changed files [8467] as expected${NC}"
-else 
-    echo "${RED}TESTCASE 3.2 did not pass - unexpected number of changed files${NC}"
-fi
-
-
-
-echo""
-
-if grep -q 'Network nodes (developers): 256' testResults.tmp; then
-    echo "${GREEN}TESTCASE 3.3 - number of nodes [279] as expected${NC}"
-else 
-    echo "${RED}TESTCASE 3.3 did not pass - unexpected number of nodes${NC}"
-fi
-
-echo""
-
-if grep -q 'Network edges (collaborations): 3363' testResults.tmp; then
-    echo "${GREEN}TESTCASE 3.4 - number of edges [3363] as expected${NC}"
-else 
-    echo "${RED}TESTCASE 3.4 did not pass - unexpected number of edges ${NC}"
-fi
-
-
-
-
-
-rm testResults.tmp
-#rm $GraphMLFILE
 
 
 
